b0b34e412cf81ffb13dd782170d5b1424a1a24d9
Felix Hortelano <felix.hortelano@est.fib.upc.edu>
2016-11-24 15:20:36 +0100

    nothing

--- src/database/inserts.py
+++ src/database/inserts.py
@@ -4,16 +4,14 @@ from neo4jrestclient.client import GraphDatabase
 def inserts(db: GraphDatabase):
     print('Inserts: START')
 
-    part = db.labels.create('Part')
+    part = db.labels.create('Part' {partkey : "1", mfgr : "aaaa", type: 'A', size: 10 })
     supplier = db.labels.create('Supplier')
-    partsupp = db.labels.create('PartSupplier')
-    nation = db.labels.create('Nation')
-    region = db.labels.create('Region')
-    customer = db.labels.create('Customer')
     line_item = db.labels.create('LineItem')
     order = db.labels.create('Order')
 
     # TODO Create nodes and add into the labels
     # TODO Create relationships between nodes
+
+
 
     return db

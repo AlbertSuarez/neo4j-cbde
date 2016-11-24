224c787109041f8cb7fed01203a433b2f887bdc9
Felix Hortelano <felix.hortelano@est.fib.upc.edu>
2016-11-24 21:21:15 +0100

    some inserts not completed

--- neo4j_teamG.py
+++ neo4j_teamG.py
@@ -1,15 +1,22 @@
 from neo4j.v1 import GraphDatabase, basic_auth
+import datetime
 
 
 def inserts(db):
     print('Inserts: START')
     session = db.session()
-    # 4 port 2 supplier 6 lineitem 2 orders
-    session.run("CREATE (a:Part {partkey:'1', mfgr:'aaaa', type: 'A', size: 10})")
-    session.run("CREATE (b:Part {partkey:'2', mfgr:'bbbb', type: 'B', size: 15})")
-    session.run("CREATE (c:Part {partkey:'3', mfgr:'aaaa', type: 'A', size: 5})")
-    session.run("CREATE (d:Part {partkey:'4', mfgr:'cccc', type: 'C', size: 2})")
 
+    date = datetime.datetime(2016,11,24)
+    
+    session.run("MATCH (n) DETACH DELETE n")
+    # 4 port 2 supplier 6 lineitem 2 orders
+    session.run("CREATE (p1:Part {partkey:'1', mfgr:'aaaa', type: 'A', size: 10})")
+    session.run("CREATE (p2:Part {partkey:'2', mfgr:'bbbb', type: 'B', size: 15})")
+    session.run("CREATE (p3:Part {partkey:'3', mfgr:'aaaa', type: 'A', size: 5})")
+    session.run("CREATE (p4:Part {partkey:'4', mfgr:'cccc', type: 'C', size: 2})")
+    session.run("CREATE (s1:Supplier {name: 's1', accbal: 1.00, adress: 'Main Street', phone: '111111111', comment: 'nothing', n_name: 'Spain', r_name: 'Barcelona'})")
+    session.run("CREATE (s1:Supplier {name: 's2', accbal: 2.00, adress: 'Main Street2', phone: '222222222', comment: 'nothing2', n_name: 'Spain', r_name: 'Barcelona'})")
+    session.run("CREATE (o1:Order {orderdate: date, shippriority:'1', c_marketsegment: 'MKT1', n_name: 'Spain'})")
     session.close()
     
 
